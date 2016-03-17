9dc46505cae197fb6079758de265f98208658ee4
mesa312 <junior05_312@hotmail.com>
2016-03-17 12:23:46 -0400

    SEE COMMENTS TO FORM

--- HelloWorld/Form1.vb
+++ HelloWorld/Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        ' WHATS UP DUDE, STAY POSITIVE. WERE PASSING THIS CLASS. WILL BE A STRUGGLE BUT WORTH IT FOR GRADUATION!!!
     End Sub
 End Class
